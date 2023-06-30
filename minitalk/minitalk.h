30922957701fb5f68aa616db7ea1b01e96872ffd
Futa Watanabe <fwatanab@c1r12s9.42tokyo.jp>
2023-06-30 11:11:47 +0900

    minitalk

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,12 +6,12 @@
 /*   By: fwatanab <fwatanab@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 21:10:54 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/06/29 21:11:22 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/06/30 11:11:34 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINI_TALK_H
-# define MINI_TALK_H
+#ifndef MINITALK_H
+# define MINITALK_H
 
 # include "libft/libft.h"
 # include <signal.h>
