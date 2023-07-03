d00cd9545c94952635232bd1eac3918062ee2753
fwatanab <fwatanab@student.42tokyo.jp>
2023-07-03 11:43:24 +0900

    img_put_clear

---
+++ so_long/so_long.h
@@ -0,0 +1,17 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+# include "libft/libft.h"
+# include "minilibx/mlx.h"
+# include "minilibx/mlx_int.h"
+
+#endif
